13190d898e4cbf15461d985ea6ced65912abbf19
SDiego-PC\SDiego <sevilla.diego8@gmail.com>
2018-02-16 18:45:41 -0600

    Cambios: -Agrege los metodos en stackArrayList -Acabo de agregar operar() en singlylinkedlist, estoy probando para ver que pasa.

--- HT4/src/Operadora/StackArrayList.java
+++ HT4/src/Operadora/StackArrayList.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Operadora;
 
 import java.util.ArrayList;
@@ -12,21 +8,57 @@ import java.util.ArrayList;
  * @author anahernandez
  */
 public class StackArrayList<E> extends AbstractStack<E>{
-    private E dato;
-    private ArrayList<E> arraylist;
-    public StackArrayList()
-    {
-        
-    }
+    protected ArrayList<E> data;
+
+	public StackArrayList()
+	// post: constructs a new, empty stack
+	{
+		data = new ArrayList<E>();
+	}
+
+    @Override
+	public void push(E item)
+	// post: the value is added to the stack
+	//          will be popped next if no intervening push
+	{
+		data.add(item);
+	}
+
+    @Override
+	public E pop()
+	// pre: stack is not empty
+	// post: most recently pushed item is removed and returned
+	{
+		return data.remove(size()-1);
+	}
+
+    @Override
+	public E peek()
+	// pre: stack is not empty
+	// post: top value (next to be popped) is returned
+	{
+		return data.get(size() - 1);
+	}
+	
+    @Override
+	public int size()
+	// post: returns the number of elements in the stack
+	{
+		return data.size();
+	}
+  
+    @Override
+	public boolean empty()
+	// post: returns true if and only if the stack is empty
+	{
+		return size() == 0;
+	}
 
     @Override
     public double operar(String expresion) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
-    public void push(E item) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    
     
 }
